022641f37f80648f74be457951872ab49018efae
NAZzarii <122733889+NAZzarii@users.noreply.github.com>
2026-09-18 17:38:17 +0300

    Create app2.cs

---
+++ app2.cs
@@ -0,0 +1,69 @@
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Studio { get; set; }
+    public string Genre { get; set; }
+    public DateTime ReleaseDate { get; set; }
+   
+    public string GameMode { get; set; }
+    public long SoldCopies { get; set; }
+}
+
+using Microsoft.EntityFrameworkCore;
+
+public class GameContext : DbContext
+{
+    public DbSet<Game> Games { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GameStoreDb;Trusted_Connection=True;");
+    }
+}
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        using (var db = new GameContext())
+        {
+            db.Database.EnsureCreated();
+
+            if (!db.Games.Any())
+            {
+                db.Games.Add(new Game 
+                { 
+                    Title = "Cyberpunk 2077", 
+                    Studio = "CD Projekt Red", 
+                    Genre = "RPG", 
+                    ReleaseDate = new DateTime(2020, 12, 10),
+                    GameMode = "Однокористувацький",
+                    SoldCopies = 25000000
+                });
+                
+                db.Games.Add(new Game 
+                { 
+                    Title = "Counter-Strike 2", 
+                    Studio = "Valve", 
+                    Genre = "Shooter", 
+                    ReleaseDate = new DateTime(2023, 9, 27),
+                    GameMode = "Багатокористувацький",
+                    SoldCopies = 0
+                });
+
+                db.SaveChanges();
+            }
+
+            var games = db.Games.ToList();
+            foreach (var g in games)
+            {
+                Console.WriteLine($"{g.Title} | {g.Studio} | {g.Genre} | {g.ReleaseDate.ToShortDateString()} | {g.GameMode} | Продано: {g.SoldCopies}");
+            }
+        }
+    }
+}
